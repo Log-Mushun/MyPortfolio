2f4b2da402f7b276d3be5348ed8db92126d32e92
Michelle Cadavid <cadavidcarmona1@gmail.com>
2023-04-05 14:40:37 -0500

    loading

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import ReactLoading from "react-loading";
 
 const Preloader = () => {
   return (
-    <div>
-      <ReactLoading
-        type={"bubbles"}
-        color={"#bed2f4"}
-        height={367}
-        width={275}
-      />
+    <div className="min-h-screen relative flex md:justify-center sm:items-center">
+      <h2>Loading...</h2>
     </div>
   );
 };
