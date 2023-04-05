e63020be192fef2a451a8704113cd1e544a6f9b2
Michelle Cadavid <cadavidcarmona1@gmail.com>
2023-04-05 13:40:28 -0500

    load

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 // import components
-import React, { useState, useEffect } from "react";
 import Portfolio from "./components/Portfolio";
-import Preloader from "./components/Preloader";
 
 const App = () => {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    Promise.all([
-      fetch("./assets/images/Hero/person.png"),
-      fetch("./assets/images/Projects/person.png"),
-      fetch("./assets/images/Hireme/person.png"),
-    ]).then(() => {
-      setLoaded(true);
-    });
-  }, []);
-
   return (
     <div>
-      {loaded ? (
-        <>
-          <Portfolio />
-        </>
-      ) : (
-        <Preloader />
-      )}
+      <Portfolio />
     </div>
   );
 };
