acca4cf9c8e9886ef52904b33c662ef8a86e0378
Michelle Cadavid <cadavidcarmona1@gmail.com>
2023-04-05 18:19:07 -0500

    animated!

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Preloader = () => {
   return (
     <div className="min-h-screen relative flex md:justify-center sm:items-center">
-      <h2>Loading...</h2>
+      <motion.div
+        initial={{ scale: 1 }}
+        animate={{ scale: [1, 1.5, 1] }}
+        exit={{ scale: 1 }}
+        transition={{ duration: 2, repeat: Infinity }}
+      >
+        <h2>Loading...</h2>
+      </motion.div>
     </div>
   );
 };
